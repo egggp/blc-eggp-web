a5ec651005e41be0305c7d42498153013ebbef1d
eggplantiny <eggplantiny@gmail.com>
2021-08-06 00:03:09 +0900

    [#7] 에러메시지 정규화 - 에러 메시지 한국어로 정규화 - 모든 에러 핸들링시 message 파라미터로 출력 - 에러 클레스 정리

--- api/errors/params.error.ts
+++ api/errors/params.error.ts
@@ -4,7 +4,7 @@ import HttpError from '~/api/errors/http.error'
 export default class ParamsError extends HttpError {
   constructor (validationError: Result<ValidationError>) {
     const parameters = validationError.array().map(error => error.param).join(', ')
-    const message = `parameter [${parameters}] must be required.`
+    const message = `파라미터 [${parameters}] 는 반드시 필요합니다.`
     super(400, message)
   }
 }
