b46049782388d04c457f7ec052d03dfed7134225
eggplantiny <eggplantiny@gmail.com>
2021-08-06 09:51:43 +0900

    [#16] AppBar 구현 - Appbar 컴포넌트 구현 - Layout 에 Appbar 추가 - Appbar => login 으로 가는 기능 추가

---
+++ components/organisms/AppBar.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-app-bar
+    app
+    dark
+    color="primary"
+  >
+    <div class="app-max-width mx-auto d-flex justify-space-between">
+      <v-toolbar-title>
+        밸런스게임
+      </v-toolbar-title>
+      <v-toolbar-items>
+        <v-btn
+          v-if="!loggedIn"
+          text
+          to="/auth/login"
+          class="rounded-lg"
+        >
+          로그인
+        </v-btn>
+        <template v-else>
+          <v-menu
+            v-model="menu"
+            rounded
+            offset-y
+          >
+            <template #activator="{ on }">
+              <v-btn
+                text
+                x-large
+                class="rounded-lg"
+                v-on="on"
+              >
+                {{ user.userName }} 님
+              </v-btn>
+            </template>
+            <v-list dense>
+              <v-list-item>
+                <v-btn
+                  text
+                  block
+                  class="rounded-lg"
+                  @click="onClickLogout"
+                >
+                  로그아웃
+                </v-btn>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </template>
+      </v-toolbar-items>
+    </div>
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+import {
+  computed,
+  defineComponent, ref,
+  useContext
+} from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  name: 'AppBar',
+  setup () {
+    const { $auth } = useContext()
+    const menu = ref<boolean>(false)
+
+    const loggedIn = computed<boolean>(() => $auth.loggedIn)
+    const user = computed(() => $auth.user)
+
+    const onClickLogout = () => $auth.logout()
+
+    return {
+      onClickLogout,
+      loggedIn,
+      user,
+      menu
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,8 @@
 <template>
   <v-app>
+    <app-bar />
     <v-main>
-      <v-container style="max-width: 800px">
+      <v-container class="app-max-width">
         <Nuxt />
       </v-container>
     </v-main>
@@ -9,10 +10,16 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from '@nuxtjs/composition-api'
+import {
+  defineComponent
+} from '@nuxtjs/composition-api'
+import AppBar from '~/components/organisms/AppBar.vue'
 
 export default defineComponent({
   name: 'DefaultLayout',
+  components: {
+    AppBar
+  },
   setup () {
     return {
     }
